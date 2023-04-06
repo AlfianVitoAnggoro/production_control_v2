8a1df6b66daeaad828d2039b3ae7d8d734b3c1d1
Luky Mulana <lukymulana@gmail.com>
2023-04-06 14:48:35 +0700

    update dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -28,6 +28,7 @@ class Dashboard extends BaseController
         $jenis_dashboard = $this->request->getPost('jenis_dashboard');
         $parent_filter = $this->request->getPost('parent_filter');
         $child_filter = $this->request->getPost('child_filter');
+        $baby_filter = $this->request->getPost('baby_filter');
         $bulan = $this->request->getPost('bulan');
 
         if ($jenis_dashboard == null) {
@@ -42,6 +43,10 @@ class Dashboard extends BaseController
             $child_filter = 0;
         }
 
+        if ($baby_filter == null) {
+            $baby_filter = 'average';
+        }
+
         if ($bulan == null) {
             $bulan = date('Y-m');
         }
@@ -54,7 +59,10 @@ class Dashboard extends BaseController
         $data['jenis_dashboard'] = $jenis_dashboard;
         $data['parent_filter'] = $parent_filter;
         $data['child_filter'] = $child_filter;
+        $data['baby_filter'] = $baby_filter;
         $data['bulan'] = $bulan;
+
+        $data['data_all_line'] = [];
 
         $data['data_line_1'] = [];
         $data['data_line_2'] = [];
@@ -70,7 +78,24 @@ class Dashboard extends BaseController
 
         if ($jenis_dashboard == 1 AND ($parent_filter == 'line' OR $parent_filter == null) AND ($child_filter == null OR $child_filter == 0)) {
             while (strtotime($start) <= strtotime($now)) {
-                $data1 = $this->M_Dashboard->get_data_all_line($start, 1);
+                $data_all = $this->M_Dashboard->get_data_all_line_by_date($start);
+                if (!empty($data_all)) {
+                    foreach ($data_all as $da) {
+                        $total_plan = $da['total_plan'];
+                        $total_aktual = $da['total_aktual'];
+                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
+                        array_push($data['data_all_line'], (float) number_format($eff, 2, '.', ''));
+                    } 
+                } else {
+                    array_push($data['data_all_line'], 0);
+                }
+    
+                $start = date ("Y-m-d", strtotime("+1 days", strtotime($start)));
+                
+            }
+        } elseif($jenis_dashboard == 1 AND ($parent_filter == 'line' OR $parent_filter == null) AND ($child_filter != null OR $child_filter != 0) AND $baby_filter != null AND $baby_filter == 'average') {
+            while (strtotime($start) <= strtotime($now)) {
+                $data1 = $this->M_Dashboard->get_data_all_line($start, $child_filter);
                 if (!empty($data1)) {
                     foreach ($data1 as $d1) {
                         $total_plan = $d1['total_plan'];
@@ -79,81 +104,8 @@ class Dashboard extends BaseController
                         array_push($data['data_line_1'], (float) number_format($eff, 2, '.', ''));
                     } 
                 } else {
-                    array_push($data['data_line_1'], 0);
+                    array_push($data['data_line_'.$child_filter], 0);
                 }
-    
-                $data2 = $this->M_Dashboard->get_data_all_line($start, 2);
-                if (!empty($data2)) {
-                    foreach ($data2 as $d2) {
-                        $total_plan = $d2['total_plan'];
-                        $total_aktual = $d2['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_2'], (float) number_format($eff, 2, '.', ''));
-                    }
-                } else {
-                    array_push($data['data_line_2'], 0);
-                }
-    
-                $data3 = $this->M_Dashboard->get_data_all_line($start, 3);
-                if (!empty($data3)) {
-                    foreach ($data3 as $d3) {
-                        $total_plan = $d3['total_plan'];
-                        $total_aktual = $d3['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_3'], (float) number_format($eff, 2, '.', ''));
-                    } 
-                } else {
-                    array_push($data['data_line_3'], 0);
-                }
-    
-                $data4 = $this->M_Dashboard->get_data_all_line($start, 4);
-                if (!empty($data4)) {
-                    foreach ($data4 as $d4) {
-                        $total_plan = $d4['total_plan'];
-                        $total_aktual = $d4['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_4'], (float) number_format($eff, 2, '.', ''));
-                    }
-                } else {
-                    array_push($data['data_line_4'], 0);
-                }
-    
-                $data5 = $this->M_Dashboard->get_data_all_line($start, 5);
-                if (!empty($data5)) {
-                    foreach ($data5 as $d5) {
-                        $total_plan = $d5['total_plan'];
-                        $total_aktual = $d5['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_5'], (float) number_format($eff, 2, '.', ''));
-                    } 
-                }else {
-                    array_push($data['data_line_5'], 0);
-                }
-    
-                $data6 = $this->M_Dashboard->get_data_all_line($start, 6);
-                if (!empty($data6)) {
-                    foreach ($data6 as $d6) {
-                        $total_plan = $d6['total_plan'];
-                        $total_aktual = $d6['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_6'], (float) number_format($eff, 2, '.', ''));
-                    } 
-                } else {
-                    array_push($data['data_line_6'], 0);
-                }
-    
-                $data7 = $this->M_Dashboard->get_data_all_line($start, 7);
-                if (!empty($data7)) {
-                    foreach ($data7 as $d7) {
-                        $total_plan = $d7['total_plan'];
-                        $total_aktual = $d7['total_aktual'];
-                        $eff = (!empty($total_plan) && !empty($total_aktual)) ? ($total_aktual / $total_plan) * 100 : 0;
-                        array_push($data['data_line_7'], (float) number_format($eff, 2, '.', ''));
-                    } 
-                } else {
-                    array_push($data['data_line_7'], 0);
-                }
-    
                 $start = date ("Y-m-d", strtotime("+1 days", strtotime($start)));
                 
             }
@@ -202,7 +154,7 @@ class Dashboard extends BaseController
 
         $data['data_all_month'] = [];
         for ($i=1; $i < 12; $i++) { 
-            $data_all = $this->M_Dashboard->get_data_all_line_by_month($i);
+            $data_all = $this->M_Dashboard->get_data_all_line_by_month($i, $child_filter);
             if (!empty($data_all)) {
                 foreach($data_all as $d_all) {
                     $total_plan = $d_all['total_plan'];
@@ -214,7 +166,7 @@ class Dashboard extends BaseController
                 array_push($data['data_all_month'], 0);
             }
         }
-        $data_year = $this->M_Dashboard->get_data_all_line_by_year();
+        $data_year = $this->M_Dashboard->get_data_all_line_by_year($child_filter);
         $data['data_all_year'] = (!empty($data_year[0]['total_plan']) && !empty($data_year[0]['total_aktual'])) ? (float) number_format(($data_year[0]['total_aktual'] / $data_year[0]['total_plan']) * 100, 2, '.', '') : 0;
         
         return view('dashboard/dashboard_lhp_assy', $data);
--- app/Models/M_Dashboard.php
+++ app/Models/M_Dashboard.php
@@ -36,36 +36,75 @@ class M_Dashboard extends Model
         return $query->getResultArray();
     }
 
-    public function get_data_all_line_by_month($bulan) 
+    public function get_data_all_line_by_date($tanggal)
     {
-        $query = $this->db->query('SELECT MONTH(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
-                                    FROM lhp_produksi2 
-                                    WHERE MONTH(tanggal_produksi) = '.$bulan.'
-                                    GROUP BY MONTH(tanggal_produksi)
-                                    ORDER BY MONTH(tanggal_produksi)
-                                    ');
+        $query = $this->db->query('SELECT tanggal_produksi, SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
+                                    FROM		lhp_produksi2
+                                    WHERE		tanggal_produksi = \''.$tanggal.'\'
+                                    GROUP BY	tanggal_produksi');
 
         return $query->getResultArray();
     }
 
-    public function get_data_all_line_by_year() 
+    public function get_data_all_line_by_month($bulan, $line) 
+    {
+        if ($line == null || $line == 0) {
+            $query = $this->db->query('SELECT MONTH(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
+                                        FROM lhp_produksi2 
+                                        WHERE MONTH(tanggal_produksi) = '.$bulan.'
+                                        GROUP BY MONTH(tanggal_produksi)
+                                        ORDER BY MONTH(tanggal_produksi)
+                                    ');
+        } else {
+            $query = $this->db->query('SELECT MONTH(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
+                                        FROM lhp_produksi2 
+                                        WHERE MONTH(tanggal_produksi) = '.$bulan.' AND line = '.$line.'
+                                        GROUP BY MONTH(tanggal_produksi)
+                                        ORDER BY MONTH(tanggal_produksi)
+                                    ');
+        }
+
+        
+
+        return $query->getResultArray();
+    }
+
+    public function get_data_all_line_by_year($line) 
     {
         $tahun = date('Y');
-        $query = $this->db->query('SELECT YEAR(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
-                                    FROM lhp_produksi2 
-                                    WHERE YEAR(tanggal_produksi) = '.$tahun.'
-                                    GROUP BY YEAR(tanggal_produksi)
-                                    ORDER BY YEAR(tanggal_produksi)
+
+        if ($line == null || $line == 0) {
+            $query = $this->db->query('SELECT YEAR(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
+                                        FROM lhp_produksi2 
+                                        WHERE YEAR(tanggal_produksi) = '.$tahun.'
+                                        GROUP BY YEAR(tanggal_produksi)
+                                        ORDER BY YEAR(tanggal_produksi)
                                     ');
+        } else {
+            $query = $this->db->query('SELECT YEAR(tanggal_produksi) AS month,SUM(total_plan) AS total_plan, SUM(total_aktual) AS total_aktual
+                                        FROM lhp_produksi2 
+                                        WHERE YEAR(tanggal_produksi) = '.$tahun.' AND line = '.$line.'
+                                        GROUP BY YEAR(tanggal_produksi)
+                                        ORDER BY YEAR(tanggal_produksi)
+                                    ');
+        }
+
+        
 
         return $query->getResultArray();
     }
 
     public function get_data_line_stop($tanggal, $line)
     {
+        if (!empty($line)) {
+            $sql = 'AND lhp_produksi2.line = '.$line;
+        } else {
+            $sql = '';
+        }
+        
         $query = $this->db->query('SELECT * FROM lhp_produksi2
                                     JOIN detail_breakdown ON detail_breakdown.id_lhp = lhp_produksi2.id_lhp_2
-                                    WHERE lhp_produksi2.tanggal_produksi = \''.$tanggal.'\' AND lhp_produksi2.line = '.$line);
+                                    WHERE lhp_produksi2.tanggal_produksi = \''.$tanggal.'\''.$sql);
 
         return $query->getResultArray();
     }
