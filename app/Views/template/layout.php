4de25e8f293212dcd025da9e4e1a1be5c6643641
Luky Mulana <lukymulana@gmail.com>
2023-05-20 09:44:03 +0700

    remove footer

--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -31,7 +31,6 @@
 		<?= $this->include('template/header') ?>
 		<?= $this->include('template/navbar') ?>
 		<?= $this->renderSection('content') ?>
-		<?= $this->include('template/footer') ?>
 
 		<!-- Control Sidebar -->
 		<aside class="control-sidebar">
