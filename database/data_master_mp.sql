33436d73c5ad15121a6766568d7a465fb065b1aa
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2023-07-05 11:53:01 +0700

    add column status to table db

--- database/data_master_mp.sql
+++ database/data_master_mp.sql
@@ -15,3 +15,42 @@ CREATE TABLE [dbo].[master_data_man_power](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+/****** Object:  Table [dbo].[detail_record_master_group_man_power]    Script Date: 05/07/2023 11:51:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[detail_record_master_group_man_power](
+	[id_record] [int] IDENTITY(1,1) NOT NULL,
+	[sub_bagian] [varchar](50) NULL,
+	[tanggal] [date] NULL,
+	[line] [int] NULL,
+	[shift] [int] NULL,
+	[group_mp] [varchar](50) NULL,
+	[mesin] [varchar](100) NULL,
+	[nama] [int] NULL,
+	[status] [varchar](50) NULL,
+	[created_at] [datetime] NULL,
+ CONSTRAINT [PK_detail_record_master_group_man_power] PRIMARY KEY CLUSTERED 
+(
+	[id_record] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[detail_record_master_group_man_power_indirect]    Script Date: 05/07/2023 11:51:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[detail_record_master_group_man_power_indirect](
+	[id_record] [int] IDENTITY(1,1) NOT NULL,
+	[sub_bagian] [varchar](50) NULL,
+	[tanggal] [date] NULL,
+	[shift] [int] NULL,
+	[group_mp] [varchar](50) NULL,
+	[mesin] [varchar](50) NULL,
+	[nama] [int] NULL,
+	[status] [varchar](50) NULL,
+	[created_at] [datetime] NULL
+) ON [PRIMARY]
+GO
